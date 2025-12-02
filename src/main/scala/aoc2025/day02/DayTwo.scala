a97bba7f4c03554ec00797c3782a259c44160077
Guillaume Vandecasteele <guillaume.vandecasteele@mow.vlaanderen.be>
2025-12-02 16:15:24 +0100

    AOC-2025: Solution day 2, part 2

--- src/main/scala/aoc2025/day02/DayTwo.scala
+++ src/main/scala/aoc2025/day02/DayTwo.scala
@@ -9,27 +9,35 @@ object DayTwo extends DailyChallenge[BigInt]:
 
   override lazy val day: LocalDate = LocalDate.of(2025, 12, 2)
 
-  override def partOne(input: Seq[String]): BigInt = input.headOption.toSeq.flatMap(parseInput).map(sumOfInvalidIds).sum
+  override def partOne(input: Seq[String]): BigInt = processInput(input, isValidIdPart1)
 
-  override def partTwo(input: Seq[String]): BigInt = 0
+  override def partTwo(input: Seq[String]): BigInt = processInput(input, isValidIdPart2)
 
   @main def run(): Unit = evaluate()
 
   private type BigRange = NumericRange[Long]
 
-  lazy val sumOfInvalidIds: BigRange => BigInt = range => range.filterNot(isValidId).sum
+  private def processInput(input: Seq[String], validationFunction: Long => Boolean): BigInt =
+    input.headOption.toSeq.flatMap(parseInput).map(sumOfInvalidIds(_, validationFunction)).sum
+  end processInput
+
+  def sumOfInvalidIds(range: BigRange, validationFunction: Long => Boolean): BigInt = range.filterNot(validationFunction).sum
   end sumOfInvalidIds
 
-  val isValidId: Long => Boolean = _.toString match
+  val isValidIdPart1: Long => Boolean = _.toString match
     case id if id.length % 2 != 0 => true
     case id if id.distinct.length == 1 => false
     case id                            => id.grouped(id.length / 2).toSeq match
         case Seq(first, second) if first == second => false
         case _                                     => true
-  end isValidId
+  end isValidIdPart1
 
-  lazy val parseInput: String => Seq[BigRange] = _.split(",").toIndexedSeq
-    .map(_.strip)
+  val isValidIdPart2: Long => Boolean = _.toString match
+    case id if id.length > 1 && id.distinct.length == 1 => false
+    case id                                             => !(2 to id.length / 2).map(id.grouped).exists(_.toSet.size == 1)
+  end isValidIdPart2
+
+  private val parseInput: String => Seq[BigRange] = _.split(",").toIndexedSeq
     .flatMap:
       case s"$start-$end" =>
         for
--- src/test/scala/aoc2025/day02/DayTwoTest.scala
+++ src/test/scala/aoc2025/day02/DayTwoTest.scala
@@ -7,28 +7,42 @@ class DayTwoTest extends BaseTest:
   private val input: Seq[String] =
     Seq("11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124")
 
-  "Processing range" must:
-    "correctly identify sequences of digits repeating twice" in:
-      DayTwo.isValidId(55) mustBe false
-      DayTwo.isValidId(6464) mustBe false
-      DayTwo.isValidId(123123) mustBe false
-      DayTwo.isValidId(101) mustBe true
-      DayTwo.isValidId(123456) mustBe true
-      DayTwo.isValidId(111111) mustBe false
-      DayTwo.isValidId(1001001) mustBe true
-
-    "correctly process entire range" in:
-      DayTwo.sumOfInvalidIds(11L to 22L) mustBe 33
-      DayTwo.sumOfInvalidIds(1188511880L to 1188511890L) mustBe 1188511885
-
   "Parsed input in part one" must:
 
+    "correctly identify sequences of digits repeating twice" in:
+      DayTwo.isValidIdPart1(55) mustBe false
+      DayTwo.isValidIdPart1(6464) mustBe false
+      DayTwo.isValidIdPart1(123123) mustBe false
+      DayTwo.isValidIdPart1(101) mustBe true
+      DayTwo.isValidIdPart1(123456) mustBe true
+      DayTwo.isValidIdPart1(111111) mustBe false
+      DayTwo.isValidIdPart1(1001001) mustBe true
+
+    "correctly process entire range" in:
+      DayTwo.sumOfInvalidIds(11L to 22L, DayTwo.isValidIdPart1) mustBe 33
+      DayTwo.sumOfInvalidIds(1188511880L to 1188511890L, DayTwo.isValidIdPart1) mustBe 1188511885
+
     "calculate the correct output" in:
-      DayTwo.partOne(input) mustBe 1227775554
+      DayTwo.partOne(input) mustBe 1227775554L
 
   "Parsed input in part two" must:
 
+    "correctly identify sequences of digits repeating twice" in:
+      DayTwo.isValidIdPart2(1) mustBe true
+      DayTwo.isValidIdPart2(55) mustBe false
+      DayTwo.isValidIdPart2(6464) mustBe false
+      DayTwo.isValidIdPart2(123123) mustBe false
+      DayTwo.isValidIdPart2(101) mustBe true
+      DayTwo.isValidIdPart2(123456) mustBe true
+      DayTwo.isValidIdPart2(111111) mustBe false
+      DayTwo.isValidIdPart2(1001001) mustBe true
+      DayTwo.isValidIdPart2(111) mustBe false
+      DayTwo.isValidIdPart2(123123123) mustBe false
+      DayTwo.isValidIdPart2(1212121212) mustBe false
+      DayTwo.isValidIdPart2(1111111) mustBe false
+      DayTwo.isValidIdPart2(444555444555L) mustBe false
+
     "calculate the correct output" in:
-      DayTwo.partTwo(input) mustBe 0
+      DayTwo.partTwo(input) mustBe 4174379265L
 
 end DayTwoTest
