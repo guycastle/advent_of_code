6f79876e9d771f6518169a87fb4e2fced71b6b85
Guillaume Vandecasteele <guillaume.vandecasteele@mow.vlaanderen.be>
2025-12-11 17:06:26 +0100

    AOC-2025: Solution day 11, part 2

--- src/main/scala/aoc2025/day11/DayEleven.scala
+++ src/main/scala/aoc2025/day11/DayEleven.scala
@@ -3,43 +3,75 @@ package aoc2025.day11
 import utils.DailyChallenge
 
 import java.time.LocalDate
-
-object DayEleven extends DailyChallenge[Int]:
+object DayEleven extends DailyChallenge[Long]:
 
   override lazy val day: LocalDate = LocalDate.of(2025, 12, 11)
 
-  override def partOne(input: Seq[String]): Int =
+  override def partOne(input: Seq[String]): Long =
     val devices     = parseInput(input)
-    val (result, _) = cachedPathCount(deviceId = in, devices = devices)
+    val (result, _) = cachedPathCount(deviceId = inPartOne, devices = devices)
     result
   end partOne
 
-  override def partTwo(input: Seq[String]): Int = 0
+  override def partTwo(input: Seq[String]): Long =
+    val devices     = parseInput(input)
+    val (result, _) = cachedPaths(deviceId = inPartTwo, devices = devices)
+    result
+  end partTwo
 
   @main def run(): Unit = evaluate()
 
-  opaque type DeviceId = String
+  private type DeviceId = String
 
-  type ResultAndCache = (result: Int, cache: Map[DeviceId, Int])
+  private type ResultAndDeviceIdCache = (result: Long, cache: Map[DeviceId, Long])
 
-  private val in: DeviceId  = "you"
-  private val out: DeviceId = "out"
+  private val inPartOne: DeviceId            = "you"
+  private val inPartTwo: DeviceId            = "svr"
+  private val requiredDevices: Set[DeviceId] = Set("dac", "fft")
+  private val out: DeviceId                  = "out"
 
   private def cachedPathCount(
       deviceId: DeviceId,
       devices: Map[DeviceId, Set[DeviceId]],
-      cache: Map[DeviceId, Int] = Map.empty): ResultAndCache =
+      cache: Map[DeviceId, Long] = Map.empty): ResultAndDeviceIdCache =
     lazy val deviceOutputs = devices.getOrElse(deviceId, Set.empty)
     cache.get(deviceId) match
       case Some(count)                         => (count, cache)
       case None if deviceOutputs.isEmpty       => (0, cache.updated(deviceId, 0))
       case None if deviceOutputs.contains(out) => (1, cache.updated(deviceId, 1))
-      case None                                => deviceOutputs.foldLeft((0, cache)):
+      case None                                => deviceOutputs.foldLeft((0L, cache)):
           case ((accResult, accCache), outputDeviceId) =>
             val (res, updatedCache) = cachedPathCount(outputDeviceId, devices, accCache)
             (accResult + res, updatedCache)
     end match
   end cachedPathCount
+
+  private type ContextCacheKey       = (DeviceId, Set[DeviceId])
+  private type ResultAndContextCache = (result: Long, cache: Map[ContextCacheKey, Long])
+
+  private def cachedPaths(
+      deviceId: DeviceId,
+      collected: Set[DeviceId] = Set.empty,
+      devices: Map[DeviceId, Set[DeviceId]],
+      cache: Map[ContextCacheKey, Long] = Map.empty): ResultAndContextCache =
+    val currentCollected = if requiredDevices.contains(deviceId) then collected + deviceId else collected
+    val cacheKey         = (deviceId, currentCollected)
+
+    cache.get(cacheKey) match
+      case Some(count) => (count, cache)
+      case None        => devices.get(deviceId) match
+          case Some(deviceOutputs) if deviceOutputs.contains(out) =>
+            val result = if requiredDevices.subsetOf(currentCollected) then 1 else 0
+            (result, cache.updated(cacheKey, result))
+          case Some(deviceOutputs) if deviceOutputs.nonEmpty =>
+            val (finalResult, finalCache) = deviceOutputs.foldLeft((0L, cache)):
+              case ((accResult, accCache), outputDeviceId) =>
+                val (res, updatedCache) = cachedPaths(outputDeviceId, currentCollected, devices, accCache)
+                (accResult + res, updatedCache)
+            (finalResult, finalCache.updated(cacheKey, finalResult))
+          case _ => (0, cache.updated(cacheKey, 0))
+    end match
+  end cachedPaths
 
   private val parseInput: Seq[String] => Map[DeviceId, Set[DeviceId]] = _.collect:
     case s"$id: $outputs" => id -> outputs.split("\\s").toSet
--- src/test/scala/aoc2025/day11/DayElevenTest.scala
+++ src/test/scala/aoc2025/day11/DayElevenTest.scala
@@ -4,7 +4,7 @@ import utils.BaseTest
 
 class DayElevenTest extends BaseTest:
 
-  private val input: Seq[String] = """aaa: you hhh
+  private val inputPart1: Seq[String] = """aaa: you hhh
                                      |you: bbb ccc
                                      |bbb: ddd eee
                                      |ccc: ddd eee fff
@@ -15,14 +15,28 @@ class DayElevenTest extends BaseTest:
                                      |hhh: ccc fff iii
                                      |iii: out""".stripMargin.split("\n").toIndexedSeq
 
+  private val inputPart2: Seq[String] = """svr: aaa bbb
+                                          |aaa: fft
+                                          |fft: ccc
+                                          |bbb: tty
+                                          |tty: ccc
+                                          |ccc: ddd eee
+                                          |ddd: hub
+                                          |hub: fff
+                                          |eee: dac
+                                          |dac: fff
+                                          |fff: ggg hhh
+                                          |ggg: out
+                                          |hhh: out""".stripMargin.split("\n").toIndexedSeq
+
   "Parsed input in part one" must:
 
     "calculate the correct output" in:
-      DayEleven.partOne(input) mustBe 5
+      DayEleven.partOne(inputPart1) mustBe 5
 
   "Parsed input in part two" must:
 
     "calculate the correct output" in:
-      DayEleven.partTwo(input) mustBe 0
+      DayEleven.partTwo(inputPart2) mustBe 2
 
 end DayElevenTest
